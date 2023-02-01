950441382f4179cea467d241260552624366ade7
nikita22001 <118523343+nikita22001@users.noreply.github.com>
2023-02-01 11:25:17 +0200

    some changes in index

--- layout/ex1.php
+++ layout/ex1.php
@@ -17,4 +17,39 @@ $title = "Exercise 1";
     <h3>Current Date</h3>
         <?php echo date("d.m.Y") ?>
 
+    <h1>3.4 $title = "PHP is interesting”. Put this variable as a title marked as h1 (heading 1) in your HTML document.</h1>
+    <?php 
+        $title1 ="PHP is interesting ";
+        echo "<h1>".$title1."</h1>";
+    ?>
+
+
+    <h1>3.5 $g1=5, $g2=4, $g3=5. These are the grades for 3 students in the course. Use HTML table into echo and include 3 columns S.n., Name, and grade.</h1>
+    <?php
+        $g1 = 5;
+        $g2 = 4;
+        $g3 = 5;
+
+        echo "<table>
+            <tr>
+                <th>S.N</th><th>Name</th><th>Grade</th>
+            </tr>
+            <tr>
+                <th>1</th><th>Pekka</th><th> $g1 </th>
+            </tr>
+            <tr>
+                <th>2</th><th>Johanna</th><th> $g2 </th>
+            </tr>
+            <tr>
+                <th>3</th><th>John</th><th> $g3 </th>
+            </tr>
+        </table>"
+    ?>
+
+    <h1> Screen Shoot </h1>
+
+    <img src="layout/Screenshot.png" alt="screenshot"> 
+
+
+
 <?php include "footer.php" ?>
